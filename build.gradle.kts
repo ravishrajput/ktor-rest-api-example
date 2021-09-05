3935fc3d0b7a22660d2b791e852466c1dbfe4e05
Ravish Rajput <ravish.rajput22@gmail.com>
2021-09-06 00:19:32 +0530

    README file added

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
     implementation("com.h2database:h2:$h2Version")
+    implementation("mysql:mysql-connector-java:8.0.25")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
 }
--- src/test/kotlin/com/ravishrajput/ApplicationTest.kt
+++ src/test/kotlin/com/ravishrajput/ApplicationTest.kt
@@ -1,18 +1,15 @@
 package com.ravishrajput
 
-import io.ktor.routing.*
+import com.ravishrajput.restapi.database.DaoRepository
+import com.ravishrajput.restapi.routing.configureRouting
 import io.ktor.http.*
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
 import kotlin.test.*
 import io.ktor.server.testing.*
-import com.ravishrajput.plugins.*
 
 class ApplicationTest {
     @Test
     fun testRoot() {
-        withTestApplication({ configureRouting() }) {
+        withTestApplication({ configureRouting(DaoRepository.create()) }) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
                 assertEquals("Hello World!", response.content)
